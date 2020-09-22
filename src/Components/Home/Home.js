cfe10c51a23f47cde2ae38edef9776148a2cdb14
Igor Kornilov <gaber32188@gmail.com>
2020-09-22 14:54:00 +0300

    simple Background parallax

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from '../../Components/UI/Button/Button';
 import sassVars from '../../Configs/Variables.scss';
 import useWindowSize from '../../Utils/CutomHooks/useWinowSize';
@@ -7,6 +7,7 @@ import styles from './Home.module.scss';
 const mobileBreakPointWidth = parseInt(sassVars['breakpoints-mobile']);
 
 export default function Home(props) {
+  const [offset, setOffSet] = useState(0);
   const { width } = useWindowSize();
   const isMobile = mobileBreakPointWidth >= width;
 
@@ -14,8 +15,22 @@ export default function Home(props) {
     window.location.href = '#portfolio';
   };
 
+  const parralaxShift = () => {
+    setOffSet(window.pageYOffset);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', parralaxShift);
+
+    return () => {
+      window.removeEventListener('scroll', parralaxShift);
+    };
+  }, []);
+
   return (
-    <section className={styles.Home}>
+    <section
+      className={styles.Home}
+      style={{ backgroundPositionY: offset * 0.5 }}>
       <h1>IGOR KORNILOV</h1>
       <h2>
         {isMobile
