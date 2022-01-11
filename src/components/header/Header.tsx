d4c3c5cd83314aee2dbce367808e244c4f6db945
Igor.Kornilov <gaber32188@gmail.com>
2022-01-11 22:53:12 +0300

    add header+suportive components

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import cn from 'classnames';
+import { SVG } from 'components/ui/svg';
+import { EmptyBtn } from 'components';
+import { Link } from 'gatsby';
+
+import * as styles from './Header.module.scss';
+
+const headerLinks = [
+  {
+    txtContent: 'PORTFOLIO',
+    href: '#portfolio',
+  },
+  {
+    txtContent: 'ABOUT',
+    href: '#about',
+  },
+  {
+    txtContent: 'RESUME',
+    href: '#resume',
+  },
+  {
+    txtContent: 'CONTACTS',
+    href: '#contacts',
+  },
+];
+
+export function Header() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const toggleNav = () => setIsNavOpen((prev) => !prev);
+
+  return (
+    <header className={styles.header}>
+      <nav className={cn(styles.nav, { [styles.nav_open]: isNavOpen })}>
+        <ul className={styles.ul}>
+          {headerLinks.map((link) => (
+            <li className={styles.li} key={link.txtContent}>
+              <Link to={link.href}>{link.txtContent}</Link>
+            </li>
+          ))}
+        </ul>
+        <EmptyBtn onClick={toggleNav} className={styles.svgBtnWrapper}>
+          <SVG.Esc />
+        </EmptyBtn>
+      </nav>
+      <EmptyBtn onClick={toggleNav} className={styles.svgBtnWrapper}>
+        <SVG.Hamburger />
+      </EmptyBtn>
+    </header>
+  );
+}
