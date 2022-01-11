e1ada46da7825706fd80e995515420b886ea770a
Igor.Kornilov <gaber32188@gmail.com>
2022-01-11 21:51:30 +0300

    style home page

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import { Button } from 'components';
+import { navigate } from 'gatsby';
+import { useViewDetection, useWindowSize } from 'hooks';
 import * as styles from './Home.module.scss';
 
 export function Home() {
+  const { isMobile } = useViewDetection();
+  const goToPortfolio = () => navigate('#portfolio');
+
   return (
     <section className={styles.home}>
       <h1>IGOR KORNILOV</h1>
-      <h2>
-        {/* {isMobile
-      ? 'FRONT END ENGINEER'
-      : 'FRONT END FOCUSED WITH FULL STACK EXPERIENCE'} */}
-      </h2>
-      <Button
-        className={styles.Btn}
-        // onClick={goToPortfolio}
-      >
+      <h2>{isMobile ? 'FRONT END ENGINEER' : 'FRONT END FOCUSED WITH FULL STACK EXPERIENCE'}</h2>
+      <Button className={styles.btn} onClick={goToPortfolio}>
         CHECK MY WORK
       </Button>
     </section>
